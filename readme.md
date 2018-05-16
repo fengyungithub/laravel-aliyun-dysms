9428d400e44edc987f5d02c7ab46075512eff89d
GROOT PAN <groot.pan@mindertech.co.uk>
2018-05-16 16:52:24 +0800

    query sms detail

--- readme.md
+++ readme.md
@@ -1,31 +1,32 @@
 ## Installation
 
-1) add the following to your composer.json. Then run `composer update`:
+1. add the following to your composer.json. Then run `composer update`:
 
 ```json
 "pgroot/laravel-aliyun-dysms": "dev-master"
 ```
 
-or
+ or
 
 ```bash
 composer require pgroot/laravel-aliyun-dysms
 ```
 
-2) Open your `config/app.php` and add the following to the `providers` array:
+2. Open your `config/app.php` and add the following to the `providers` array:
 
 ```php
 Mindertech\Dysms\MindertechDysmsServiceProvider::class
 ```
 
 
-3) In the same `config/app.php` and add the following to the `aliases ` array: 
+3. In the same `config/app.php` and add the following to the `aliases ` array: 
 
 ```php
-'SendSms' => Mindertech\Dysms\Facades\SendSmsFacade::class
+'SendSms' => Mindertech\Dysms\Facades\SendSmsFacade::class,
+'QuerySms' => Mindertech\Dysms\Facades\QuerySendDetailsFacade::class,
 ```
 
-4) Run the command below to publish the package config file `config/dysms.php`:
+4. Run the command below to publish the package config file `config/dysms.php`:
 
 ```shell
 php artisan vendor:publish --provider=Mindertech\Dysms\MindertechDysmsServiceProvider
@@ -38,6 +39,9 @@ Set the property values in the `config/dysms.php`.
 
 ## Usage
 
+
+1. send sms
+
 ```php
 try {
     $bizId = SendSms::to('SMS_123456', '18688886666', [
@@ -49,9 +53,37 @@ try {
 ```
 
 
+2. query sms 
+
+```php 
+$config = [];
+$page = 1;
+$pageSize = 1;
+$bizId = null;
+$result = QuerySms::search('18688886666', '20180516', $config, $page, $pageSize, $bizId);
+```
+
+response
+
+```json
+{
+	"page": 1,
+	"pageSize": 1,
+	"sendDate": "20180516",
+	"result": [{
+		"SendDate": "2018-05-16 16:04:53",
+		"SendStatus": 3,
+		"ReceiveDate": "2018-05-16 16:04:57",
+		"ErrCode": "0",
+		"TemplateCode": "SMS_123456",
+		"Content": "",
+		"PhoneNum": "18688886666"
+	}],
+	"total": 7
+}
+```
 
 ## Todo
 
-* Query Detail
 * Send Batch
 * SMS Queue
--- src/Mindertech/Dysms/QuerySendDetailsRequest.php
+++ src/Mindertech/Dysms/QuerySendDetailsRequest.php
@@ -7,11 +7,75 @@
 
 namespace Mindertech\Dysms;
 
+use Aliyun\Api\Sms\Request\V20170525\QuerySendDetailsRequest as AliyunQuerySendDetailsRequest;
+
+/**
+ * Class QuerySendDetailsRequest
+ * @package Mindertech\Dysms
+ */
 class QuerySendDetailsRequest {
+
+    /**
+     * @var
+     */
     public $app;
 
+    /**
+     * QuerySendDetailsRequest constructor.
+     * @param $app
+     */
     public function __construct($app)
     {
         $this->app = $app;
+    }
+
+
+    /**
+     * @param $phoneNumber
+     * @param $sendDate
+     * @param array $config
+     * @param int $page
+     * @param int $pageSize
+     * @param null $bizId
+     * @param null $protocol
+     * @return array
+     */
+    public function search($phoneNumber, $sendDate, array $config = [], $page = 1, $pageSize = 10, $bizId = null, $protocol = null)
+    {
+        $client = new AcsClient($config);
+        $config = $client->getConfig();
+
+        $request = new AliyunQuerySendDetailsRequest();
+        $request->setPhoneNumber($phoneNumber);
+        $request->setSendDate($sendDate);
+        $request->setPageSize($pageSize);
+        $request->setCurrentPage($page);
+
+        if(!is_null($bizId)) {
+            $request->setBizId($bizId);
+        }
+        if(!is_null($protocol)) {
+            $request->setProtocol($protocol);
+        }
+
+        $acsResponse = $client->getAcsClient()->getAcsResponse($request);
+
+        $status = isset($acsResponse->Code) ? $acsResponse->Code : 'ERROR';
+
+        $default = [
+            'page' => $page,
+            'pageSize' => $pageSize,
+            'sendDate' => $sendDate,
+            'result' => []
+        ];
+
+        if(strtolower($status) !== 'ok') {
+            return $default;
+        }
+
+        return array_merge($default, [
+            'total' => $acsResponse->TotalCount,
+            'result' => $acsResponse->SmsSendDetailDTOs->SmsSendDetailDTO
+        ]);
     }
 }
